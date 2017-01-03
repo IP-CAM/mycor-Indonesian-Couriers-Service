59241ea7e0af52fcb717034a05ac9f9add9a1bbc
Farid Saleh <opencartplugin@gmail.com>
2017-01-03 14:41:17 +0900

    tambahkan language dan gitignore

---
+++ admin/language/en-gb/extension/module/pro_autosuggest.php
@@ -0,0 +1,80 @@
+<?php
+// Heading
+$_['heading_title']       = 'AutoComplete Search 2.0';
+
+// Text
+$_['text_module']         = 'Modules';
+$_['text_success']        = 'Success You have modified AutoComplete Searching module!';
+
+// Entry
+$_['entry_creator']      = 'Muhammad Farid Saleh';
+$_['entry_version']      = 'Version<br /><span class="help">1.0</span>';
+$_['entry_licence']      = 'Licence<br /><span class="help">Purchase</span>';
+
+$_['entry_enable']        = 'Module Status';
+$_['entry_enable_desc']= 'Enable / Disable Module';
+
+$_['entry_status']        = 'Module Status';
+$_['entry_status_desc']= 'Enable or Disable Module';
+
+$_['entry_limit'] = 'Limit';
+$_['entry_limit_desc'] = 'Maximum number of products will display in the searching';
+$_['entry_minchar'] = 'Minimal Char';
+$_['entry_minchar_desc'] = 'Minimal Character to start the searching';
+$_['entry_wh'] = 'Image Size';
+$_['entry_wh_desc'] = 'Size of Thumbnail image in px';
+$_['entry_w_desc'] = 'Width of Thumbnail image in px';
+$_['entry_h_desc'] = 'High of Thumbnail image in px';
+$_['entry_searchfor'] = 'Search for';
+$_['entry_searchfor_desc'] = '';
+$_['entry_extrafield'] = 'Extra Field';
+$_['entry_extrafield_desc'] = 'Extra Field display';
+$_['entry_extrachar'] = 'Length Extra Field';
+$_['entry_extrachar_desc'] = 'Length Extra Field in Character';
+$_['entry_searchwidth'] = 'Width of Search Box';
+$_['entry_searchwidth_desc'] = 'Width of Autocomplete Search Box in Search Textbox % ';
+$_['entry_searchlm'] = 'Left Margin of Search Box';
+$_['entry_searchlm_desc'] = 'Left Margin of Autocomplete Search Box';
+$_['entry_exsmalldevice'] = 'Extra Small Device (<768px)';
+$_['entry_smalldevice'] = 'Small Device (>=768px)';
+$_['entry_mediumdevice'] = 'Medium Device (>=992px)';
+$_['entry_largedevice'] = 'Large Device (>=1200px)';
+$_['entry_showthumb'] = 'Show Thumbnail Image';
+$_['entry_showthumb_desc'] = '';
+$_['entry_showextra'] = 'Show Extra Field';
+$_['entry_showextra_desc'] = '';
+$_['entry_showprice'] = 'Show Product Price';
+$_['entry_showprice_desc'] = '';
+
+$_['entry_bcboxeven'] = 'Background Search Box 1st';
+$_['entry_bcboxeven_desc'] = 'Background Color Search Box (Odd)';
+$_['entry_bcboxodd'] = 'Background Search Box 2nd';
+$_['entry_bcboxodd_desc'] = 'Background Color Search Box (Even)';
+
+$_['entry_brcbox'] = 'Border Box Color';
+$_['entry_brcbox_desc'] = '';
+$_['entry_brctopbox'] = 'First Border Box Color';
+$_['entry_brctopbox_desc'] = '';
+$_['entry_selcolor'] = 'Selected Color ';
+$_['entry_selcolor_desc'] = '';
+$_['entry_excolor'] = 'Extra Field Font ';
+$_['entry_excolor_desc'] = '';
+$_['entry_pricecolor'] = 'Price Font ';
+$_['entry_pricecolor_desc'] = '';
+$_['entry_priceoldcolor'] = 'Price Old Font ';
+$_['entry_priceoldcolor_desc'] = '';
+$_['entry_titlecolor'] = 'Title Font ';
+$_['entry_titlecolor_desc'] = '';
+$_['entry_titletypecolor'] = 'Title Font when Type ';
+$_['entry_titletypecolor_desc'] = '';
+
+$_['entry_color'] = 'Color';
+$_['entry_size'] = 'Size (px)';
+$_['entry_weight'] = 'Weight';
+$_['entry_style'] = 'Style';
+$_['entry_decoration'] = 'Decoration';
+
+// Error
+$_['error_permission']    = 'Warning You do not have permission to modify module most viewed!';
+$_['error_code']         = 'Error Module!';
+?>
---
+++ admin/language/en-gb/extension/shipping/shindo.php
@@ -0,0 +1,41 @@
+<?php
+// Heading
+$_['heading_title']    = 'Indonesian Couriers Extension';
+
+// Text
+$_['text_shipping']    = 'Shipping';
+$_['text_success']     = 'Success: You have modified Indonesian Couriers Extension!';
+$_['text_edit']        = 'Edit Indonesian Couriers Extension';
+$_['text_extension']   = 'Extensions';
+// Entry
+$_['tab_igstiki']       = 'TIKI';
+$_['tab_igspos']  = 'POS Indonesia';
+$_['tab_igsjne']   = 'JNE';
+
+$_['entry_service']       = 'Services';
+$_['entry_apikey']       = 'API Key';
+$_['entry_province']  = 'Origin Province';
+$_['entry_city']   = 'Origin City';
+$_['entry_handling']     = 'Handling Fee';
+$_['entry_handlingmode']     = 'Handling Fee Mode';
+$_['entry_status']     = 'Status';
+$_['entry_sort_order'] = 'Sort Order';
+$_['entry_tax_class']  = 'Tax Class';
+$_['entry_geo_zone']   = 'Geo Zone';
+$_['option_handlingmode1'] = 'Cost + Handling Fee';
+$_['option_handlingmode2'] = 'Cost + (Handling fee * weight (kg))';
+
+$_['entry_weight_class'] = 'Weight Class';
+$_['entry_tax_class']    = 'Tax Class';
+$_['entry_geo_zone']     = 'Geo Zone';
+
+/*$_['entry_rate']       = 'Rates';
+$_['entry_status']     = 'Status';
+$_['entry_sort_order'] = 'Sort Order';
+*/
+// Help
+
+$_['help_handling']        = 'Input Handling Fee in Rupiahs';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify weight based shipping!';
